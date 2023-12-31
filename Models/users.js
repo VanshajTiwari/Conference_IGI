363cf2120dfbf42083403d1e5ac30187ec7c37b5
Vanshaj Tiwari <vanshaj@vanshaj.vanshaj>
2024-01-01 02:14:54 +0530

    Add Some more Functionality

--- Models/users.js
+++ Models/users.js
@@ -2,7 +2,7 @@ const Mongoose=require("mongoose");
 const validator=require('validator');
 const bcrypt=require('bcryptjs');
 const crypto=require('crypto');
-const usersSchema=new Mongoose.Schema({
+const userSchema=new Mongoose.Schema({
     fullname:{
         type:String,
         minlength:1,
@@ -28,7 +28,8 @@ const usersSchema=new Mongoose.Schema({
                 return el===this.password
             },
             message:"Password mismatched"
-        }
+        },
+        select:false
     },
     avatar:{type:String},
     status:{type:String},
@@ -40,50 +41,69 @@ const usersSchema=new Mongoose.Schema({
     ],
     created_At:{
         type:Date,
-        default:Date.now()
+        default:Date.now(),
+        select:false
     },
     active:{
         type:Boolean,
-        default:true
+        default:true,
+        select:false
     },
     role:{
         type:String,
         enum:["participant","admin","me"],
-        default:"participant"  
+        default:"participant"  ,
+        select:false
     },
     passwordChangedAt:Date,
     passswordResetToken:String,
     passwordResetExpires:Date,
+},{
+    toJSON:{virtuals:true},
+    toObject:{virtuals:true},
 });
-usersSchema.pre('save',async function(next){
-    this.password=await bcrypt.hash(this.password,12);
-   
-    this.confirmpassword="";
-    next();
+userSchema.virtual("InstitutionDetails",{
+    ref:'institutionschema',
+    foreignField:"user",
+    localField:"_id"
+    
 });
 
-usersSchema.methods.Y1=function(){
-    console.log("Hello");
-}
-usersSchema.methods.correctPassword = async (candidatePassword,password) =>{
 
+userSchema.methods.correctPassword = async (candidatePassword,password) =>{
     try {
       return await bcrypt.compare(candidatePassword, password);
     } catch (error) {
       throw new Error(error);
     }
   };
-// usersSchema.methods.correctPassword=async function(candidatePassword,userPassword){
-//         return await bcrypt.compare(candidatePassword,userPassword);
-//       };
- usersSchema.statics.resetPassword=function(){
+ userSchema.statics.resetPassword=function(){
     const reset=crypto.randomBytes(32).toString('hex');
     this.passswordResetToken=crypto.createHash('sha256').update(reset).digest('hex');
     this.passwordResetExpires=Date.now()+10*60*1000
     return reset;
  };
+userSchema.methods.createResetToken=function(){
+    const resetToken=crypto.randomBytes(12).toString('hex');
+    this.resetToken=crypto.createHash('sha256').update(resetToken).digest('hex');
+    this.resetTokenCreatedAt=Date.now();
+    return resetToken;
 
-const Users=Mongoose.model("Users",usersSchema);
+}
+userSchema.pre('save',async function(next){
+        // if(this.isModified()){
+            
+        //    return next();
+        // }
+     
+        this.confirmpassword="",
+        this.password=await bcrypt.hash(this.password,12);
+        next();
+});
+userSchema.method('isModified',function(){
+        return Boolean(this.resetToken);
+},{suppressWarning:true});
+const Users=Mongoose.model("Users",userSchema);
 
     
 module.exports=Users; 
