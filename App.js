cefc5575542fed580c5839e6e917d4c525c6c9a1
Vanshaj Tiwari <vanshaj@vanshaj.vanshaj>
2024-01-15 20:22:10 +0530

    Rooms Created

--- App.js
+++ App.js
@@ -31,27 +31,36 @@ App.use(expressSession({
 }))
 App.use("/users",userRouter);
 App.use("/",viewRouter);
-App.use("/users",chatRouter);
+App.use("/chats",chatRouter);
 App.use("/meeting",meetingRoute);
 App.all("*",(req,res)=>{
     res.status(404).render("404page.ejs");
 });
-let rooms=new Set();
+let roomObj={};
 io.on("connection",(socket)=>{
+    socket.on("join-room",(data)=>{
+        console.log(data);
+        socket.join(data.roomID);
+        roomObj={roomID:data.roomID,userName:data.userName};
+        console.log(`${data.userName} bhai ka kamra`);
+        
+      });
+
     socket.on('user-data',(user)=>{
             console.log(user);
     });
-    socket.on('all-users',({roomID})=>{
-        rooms.add(roomID);
+    // socket.on('all-users',({roomID})=>{
+    //     rooms.add(roomID);
       
-    });
+    // });
     console.log("connected Users");
     socket.on("disconnect",()=>{console.log("disconnected")});
     socket.on('send-message',({message,senderId,user})=>{
-        socket.except(senderId).emit('receive-message',{message,user});
+    //     socket.except(senderId).emit('receive-message',{message,user});
+        io.except(senderId).to(roomObj.roomID).emit('receive-message',{message,user});
     });
 });
-
-      
+io.to(roomObj.roomID).emit('send-message-room',{message:"hello"});
+// io.to(roomObj.roomID).emit('send-message',{message,user});    
  
-module.exports={App};
+module.exports={App}; 
