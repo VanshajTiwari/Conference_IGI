3fa86aee3ae7fbc6ac95ee0b921e846b7271fc3b
Srajan Bansal <srajanbansal999@gmail.com>
2024-03-14 11:26:27 +0530

    add new functionality

--- Models/users.js
+++ Models/users.js
@@ -1,101 +1,105 @@
-const Mongoose=require("mongoose");
-const validator=require('validator');
-const bcrypt=require('bcryptjs');
-const crypto=require('crypto');
-const userSchema=new Mongoose.Schema({
-    fullname:{
-        type:String,
-        minlength:1,
-        required:[true,"Invalid Name"],
-        
-    },
-    email:{
-        type:String,
-        required:[true,"User Must Have Email"],
-        validate:[validator.isEmail,"Please Provide Valid Email"],
-    },
-    password:{ 
-        type:String,
-        required:[true,"Users Must Have Password"],
-        select:false
-    },
-    confirmpassword:{
-        type:String,
-        required:[true,"Confirm Password Field is Empty"],
-        validate:{
-            validator:function(el){
-                return el===this.password
-            },
-            message:"Password mismatched"
-        },
-        select:false
-    },
-    avatar:{type:String},
-    status:{type:String},
-    friendlist:[
-        {
-        type:Mongoose.Schema.Types.ObjectId,
-        ref:"Users",
-        }
-    ],
-    created_At:{
-        type:Date,
-        default:Date.now(),
-        select:false
-    },
-    active:{
-        type:Boolean,
-        default:true,
-        select:false
-    },
-    role:{
-        type:String,
-        enum:["participant","admin","me"],
-        default:"participant"  ,
-        select:false
-    },
-    passwordChangedAt:{
-        type:Date,
-        select:false
-    },
-    passswordResetToken:{
-        type:String,
-        select:false
-    },
-    passwordResetExpires:{
-        type:Date,
-        Select:false
-    },
-},{
-    toJSON:{virtuals:true},
-    toObject:{virtuals:true},
+const Mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
+const userSchema = new Mongoose.Schema(
+	{
+		fullname: {
+			type: String,
+			minlength: 1,
+			required: [true, 'Invalid Name'],
+		},
+		email: {
+			type: String,
+			required: [true, 'User Must Have Email'],
+			validate: [validator.isEmail, 'Please Provide Valid Email'],
+		},
+		password: {
+			type: String,
+			required: [true, 'Users Must Have Password'],
+			select: false,
+		},
+		confirmpassword: {
+			type: String,
+			// required:[true,"Confirm Password Field is Empty"],
+			validate: {
+				validator: function (el) {
+					return el === this.password;
+				},
+				message: 'Password mismatched',
+			},
+			select: false,
+		},
+		avatar: { type: String },
+		status: { type: String },
+		friendlist: [
+			{
+				type: Mongoose.Schema.Types.ObjectId,
+				ref: 'Users',
+			},
+		],
+		created_At: {
+			type: Date,
+			default: Date.now(),
+			select: false,
+		},
+		active: {
+			type: Boolean,
+			default: true,
+			select: false,
+		},
+		role: {
+			type: String,
+			enum: ['participant', 'admin', 'me'],
+			default: 'participant',
+			select: false,
+		},
+		passwordChangedAt: {
+			type: Date,
+			select: false,
+		},
+		passswordResetToken: {
+			type: String,
+			select: false,
+		},
+		passwordResetExpires: {
+			type: Date,
+			Select: false,
+		},
+	},
+	{
+		toJSON: { virtuals: true },
+		toObject: { virtuals: true },
+	}
+);
+userSchema.virtual('InstitutionDetails', {
+	ref: 'institutionschema',
+	foreignField: 'user',
+	localField: '_id',
 });
-userSchema.virtual("InstitutionDetails",{
-    ref:'institutionschema',
-    foreignField:"user",
-    localField:"_id"
-    
+userSchema.virtual('ownMeetings', {
+	ref: 'meetings',
+	foreignField: 'createdBy',
+	localField: '_id',
 });
-userSchema.virtual("ownMeetings",{
-    ref:"meetings",
-    foreignField:"createdBy",
-    localField:"_id"
-})
 
-userSchema.methods.correctPassword = async (candidatePassword,password) =>{
-    try {
-      return await bcrypt.compare(candidatePassword,password);
-    } catch (error) {
-      throw new Error(error);
-    }
-  };
- userSchema.methods.resetPassword=function(){
-    const reset=crypto.randomBytes(32).toString('hex');
-    this.passwordChangedAt=Date.now();
-    this.passswordResetToken=crypto.createHash('sha256').update(reset).digest('hex');
-    this.passwordResetExpires=Date.now()+10*60*1000;
-    return reset;
- };
+userSchema.methods.correctPassword = async (candidatePassword, password) => {
+	try {
+		return await bcrypt.compare(candidatePassword, password);
+	} catch (error) {
+		throw new Error(error);
+	}
+};
+userSchema.methods.resetPassword = function () {
+	const reset = crypto.randomBytes(32).toString('hex');
+	this.passwordChangedAt = Date.now();
+	this.passswordResetToken = crypto
+		.createHash('sha256')
+		.update(reset)
+		.digest('hex');
+	this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+	return reset;
+};
 // userSchema.methods.createResetToken=function(){
 //     const resetToken=crypto.randomBytes(12).toString('hex');
 //     this.resetToken=crypto.createHash('sha256').update(resetToken).digest('hex');
@@ -103,19 +107,18 @@ userSchema.methods.correctPassword = async (candidatePassword,password) =>{
 //     return resetToken;
 
 // }
-userSchema.pre('save',async function(next){
-        if(this.isModified()){
-           return next();
-        }
-     
-        this.confirmpassword="",
-        this.password=await bcrypt.hash(this.password,12);
-        next();
-});
-userSchema.methods.isModified=(function(){
-        return Boolean(this.passswordResetToken);
-});
-const Users=Mongoose.model("Users",userSchema);
+userSchema.pre('save', async function (next) {
+	if (this.isModified()) {
+		return next();
+	}
 
-    
-module.exports=Users; 
+	(this.confirmpassword = ''),
+		(this.password = await bcrypt.hash(this.password, 12));
+	next();
+});
+userSchema.methods.isModified = function () {
+	return Boolean(this.passswordResetToken);
+};
+const Users = Mongoose.model('Users', userSchema);
+
+module.exports = Users;
